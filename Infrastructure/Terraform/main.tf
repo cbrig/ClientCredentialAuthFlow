1191934c1b3f86fe2b76a7656a1dd635b4600bef
Chris Briggs <52322073+cbrig@users.noreply.github.com>
2021-12-02 10:28:16 +0000

    Update main.tf

--- Infrastructure/Terraform/main.tf
+++ Infrastructure/Terraform/main.tf
@@ -16,3 +16,37 @@ locals {
 		ApplicationName = "${var.application_name}"
 	}
 }
+
+resource "aws_dynamodb_table" "booking_table" {
+    name            = "booking"
+    billing_mode    = "PAY_PER_REQUEST"
+	stream_enabled   = true
+	stream_view_type = "NEW_AND_OLD_IMAGES"
+    hash_key        = "PartitionKey"
+    range_key	    = "Id"
+
+    attribute {
+        name = "PartitionKey"
+        type = "S"
+    }
+
+    attribute {
+        name = "Id"
+        type = "S"
+    }
+
+    attribute {
+        name = "Data"
+        type = "S"
+    }
+
+    global_secondary_index {
+        name               = "DataIndex"
+		hash_key           = "PartitionKey"
+        range_key          = "Data"
+		projection_type	   = "ALL"
+    }
+
+    point_in_time_recovery {
+        enabled = true
+    }
